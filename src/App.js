b63a9f3ecd7f37785852138fe41ec5578538c1c0
tiffany-seriani <tiffanyseriani85@gmail.com>
2026-08-07 10:38:36 +0300

    added profile

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React, { Component } from "react";
-
+import profile from "./components/Profile";
 
 class App extends Component {
+
+
+  constructor() {
+    super();
+
+    this.state = {
+      name: "Seriani",
+      bio: "I am an aspiring full-stack software developer currently studying at GomyCode Nairobi. Furthermore, I also bake as a side hustle. I offer a variety of cinnamon roll flavors. My goal is to become impactful as I am constantly expanding my creative and technical skills."
+    };
+  }
+
   render() {
     return (
-      <>
-      Constructor() {
-        this.state = {
-          name: "Seriani",
-          bio :"I am an aspiring full-stack software developer currently studying at GomyCode Nairobi.furthermore i also bake as a side hustle.I offer a variety of cinnamon roll flavors.My goal is to become impactful as i am constantly expanding my creative and technical skills. "
-        }
-       
-       
-    </>
-      }
+      <div>
+        <h1>{this.state.name}</h1>
+        <p>{this.state.bio}</p>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,15 +1,17 @@
 
 import React from "react";
 
+// profile component that takes a person object as a prop and displays their information
 const Profile = ({ person }) => {
   return (
     <div>
-
       <h2>{person.fullName}</h2>
 
       <p>{person.bio}</p>
 
       <h3>{person.profession}</h3>
+
+      <img src={person.imgSrc} alt={person.fullName} width="200" />
     </div>
   );
 };
